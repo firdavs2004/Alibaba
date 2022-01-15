b0392dda74a0e9952e25c92285344b59c774da7c
aafafg <abror.mansurov.wot@gmail.com>
2022-01-15 17:10:46 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,149 @@
+// Menu ni yana ko'paytirmoqchi bo'lsangiz
+//  tartib raqamlarni bir hilda bo'zgartirin shunda to'g'ri bo'ladi 
+
+
+// List1
+let List1 = document.getElementById('List1');
+let MenuList1 = document.getElementById('menuList1');
+// List2
+let List2 = document.getElementById('List2');
+let MenuList2 = document.getElementById('menuList2');
+// List3
+let List3 = document.getElementById('List3');
+let MenuList3 = document.getElementById('menuList3');
+// List4
+let List4 = document.getElementById('List4');
+let MenuList4 = document.getElementById('menuList4');
+let List5 = document.getElementById('List5');
+let MenuList5 = document.getElementById('menuList5');
+let List6 = document.getElementById('List6');
+let MenuList6 = document.getElementById('menuList6');
+
+// List1
+List1.addEventListener('mouseover', () => {
+   MenuList1.style.display = 'block'
+   List1.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List1.style.color = 'chocolate'
+})
+List1.addEventListener('mouseleave' , () => {
+    MenuList1.style.display = 'none'
+    List1.style.boxShadow = ' 1px 2px 12px white'
+    List1.style.color = 'black'
+})
+MenuList1.addEventListener('mouseenter', () => {
+   MenuList1.style.display = 'block'
+   List1.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List1.style.color = 'chocolate'
+})
+MenuList1.addEventListener('mouseleave' , () => {
+    MenuList1.style.display = 'none'
+    List1.style.boxShadow = ' 1px 2px 12px white'
+    List1.style.color = 'black'
+})
+
+// List2
+List2.addEventListener('mouseenter', () => {
+   MenuList2.style.display = 'block'
+   List2.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List2.style.color = 'chocolate'
+})
+List2.addEventListener('mouseleave' , () => {
+    MenuList2.style.display = 'none'
+    List2.style.boxShadow = ' 1px 2px 12px white'
+    List2.style.color = 'black'
+})
+MenuList2.addEventListener('mouseenter', () => {
+   MenuList2.style.display = 'block'
+   List2.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List2.style.color = 'chocolate'
+})
+MenuList2.addEventListener('mouseleave' , () => {
+    MenuList2.style.display = 'none'
+    List2.style.boxShadow = ' 1px 2px 12px white'
+    List2.style.color = 'black'
+})
+
+// List3
+List3.addEventListener('mouseenter', () => {
+   MenuList3.style.display = 'block'
+   List3.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List3.style.color = 'chocolate'
+})
+List3.addEventListener('mouseleave' , () => {
+    MenuList3.style.display = 'none'
+    List3.style.boxShadow = ' 1px 2px 12px white'
+    List3.style.color = 'black'
+})
+MenuList3.addEventListener('mouseenter', () => {
+   MenuList3.style.display = 'block'
+   List3.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List3.style.color = 'chocolate'
+})
+MenuList3.addEventListener('mouseleave' , () => {
+    MenuList3.style.display = 'none'
+    List3.style.boxShadow = ' 1px 2px 12px white'
+    List3.style.color = 'black'
+})
+
+// List4
+
+List4.addEventListener('mouseenter', () => {
+   MenuList4.style.display = 'block'
+   List4.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List4.style.color = 'chocolate'
+})
+List4.addEventListener('mouseleave' , () => {
+    MenuList4.style.display = 'none'
+    List4.style.boxShadow = ' 1px 2px 12px white'
+    List4.style.color = 'black'
+})
+MenuList4.addEventListener('mouseenter', () => {
+   MenuList4.style.display = 'block'
+   List4.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List4.style.color = 'chocolate'
+})
+MenuList4.addEventListener('mouseleave' , () => {
+    MenuList4.style.display = 'none'
+    List4.style.boxShadow = ' 1px 2px 12px white'
+    List4.style.color = 'black'
+})
+List5.addEventListener('mouseenter', () => {
+   MenuList5.style.display = 'block'
+   List5.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List5.style.color = 'chocolate'
+})
+List5.addEventListener('mouseleave' , () => {
+    MenuList5.style.display = 'none'
+    List5.style.boxShadow = ' 1px 2px 12px white'
+    List5.style.color = 'black'
+})
+MenuList5.addEventListener('mouseenter', () => {
+   MenuList5.style.display = 'block'
+   List5.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List5.style.color = 'chocolate'
+})
+MenuList5.addEventListener('mouseleave' , () => {
+    MenuList5.style.display = 'none'
+    List5.style.boxShadow = ' 1px 2px 12px white'
+    List5.style.color = 'black'
+})
+List6.addEventListener('mouseenter', () => {
+   MenuList6.style.display = 'block'
+   List6.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List6.style.color = 'chocolate'
+})
+List6.addEventListener('mouseleave' , () => {
+    MenuList6.style.display = 'none'
+    List6.style.boxShadow = ' 1px 2px 12px white'
+    List6.style.color = 'black'
+})
+MenuList6.addEventListener('mouseenter', () => {
+   MenuList6.style.display = 'block'
+   List6.style.boxShadow = '1px 2px 15px rgba(0, 0, 0, 00.1)'
+   List6.style.color = 'chocolate'
+})
+MenuList6.addEventListener('mouseleave' , () => {
+    MenuList6.style.display = 'none'
+    List6.style.boxShadow = ' 1px 2px 12px white'
+    List6.style.color = 'black'
+})
